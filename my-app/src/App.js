77e4e943df272351f0ebe65dd09c9345e557cd7e
ShathaAlmansour <>
2021-12-24 19:15:20 +0300

    password

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,9 @@ import Login from "./components/Login"
 import Register from "./components/Register";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import PasswordReset from "./components/Passwordrest";
+import Activated from "./components/Activated";
+import Forget from "./components/Forget";
 
 function App() {
   return (
@@ -13,7 +16,9 @@ function App() {
       <Route exact path="/Home" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
-
+        <Route exact path="/forgot/:token" element={<PasswordReset />} />
+        <Route exact path="/forgot" element={<Forget />} />
+        <Route exact path="/activate/:token" element={<Activated />} />
       </Routes>
     </>
   );
