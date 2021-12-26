e8becf9a0e15bf5728e2bc21c825e6880a2b61c8
ShathaAlmansour <>
2021-12-26 10:16:45 +0300

    Advices

--- my-app/src/components/Home/index.js
+++ my-app/src/components/Home/index.js
@@ -1,43 +1,11 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from 'react'
 
 const Home = () => {
-  const BASE_URL = process.env.REACT_APP_BASE_URL;
-  const [userInfo, setuserInfo] = useState([]);
-  const [status, setStatus] = useState("");
-
-  const getPost = () => {
-    try {
-      axios
-        .get(`${BASE_URL}/getInfo/61c37b42ff433fb0f9c1ef92`)
-        .then((result) => {
-          console.log(result.data);
-          setuserInfo(result.data);
-          let s = result.data.height / result.data.weight;
-          console.log(s);
-          if (s > 5 && s < 7) setStatus("normal");
-          if (s > 1 && s < 3) setStatus("underweight");
-          if (s > 5 && s < 10) setStatus("overweight");
-          if (s > 3 && s < 5) setStatus("obesity");
-        });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getPost();
-  }, []);
-
   return (
     <div>
-      Your calories: {userInfo.weight * 24 * userInfo.active}
-      <br />
-      Status: {status}
-      <br />
-      Your Water: {userInfo.weight * 30}
+      Home
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
